f4a1e8f34f8bea6acfe04092161da8ec1f13cc04
Juan Montiel <3518110531@uth.edu.mx>
2021-08-14 01:24:30 -0500

    Initial commit

---
+++ mobile/src/main/java/com/jp/wear/phone/mywear/MainActivity.java
@@ -0,0 +1,174 @@
+package com.jp.wear.phone.mywear;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.wearable.CapabilityClient;
+import com.google.android.gms.wearable.CapabilityInfo;
+import com.google.android.gms.wearable.DataClient;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.MessageClient;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+
+public class MainActivity extends Activity
+        implements DataClient.OnDataChangedListener,
+                /*MessageClient.OnMessageReceivedListener,
+                CapabilityClient.OnCapabilityChangedListener,*/
+                View.OnClickListener{
+
+    String datapath = "/data_path";
+    Button mybutton;
+    TextView logger, txtHr, txtTemp;
+    String TAG = "Mobile MainActivity";
+    int num = 1;
+    String m = "H";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //get the widgets
+        mybutton = findViewById(R.id.btnSend);
+        mybutton.setOnClickListener(this);
+        logger = findViewById(R.id.logger);
+        txtHr = findViewById(R.id.txtHr);
+        txtTemp = findViewById(R.id.txtTemp);
+    }
+
+    // add data listener
+    @Override
+    public void onResume() {
+        super.onResume();
+        Wearable.getDataClient(this).addListener(this);
+    }
+
+    //remove data listener
+    @Override
+    public void onPause() {
+        super.onPause();
+        Wearable.getDataClient(this).removeListener(this);
+    }
+
+    /**
+     * simple method to add the log TextView.
+     */
+    public void logthis(String newinfo) {
+        if (newinfo.compareTo("") != 0) {
+            logger.append("\n" + newinfo);
+        }
+    }
+
+    //button listener
+    @Override
+    public void onClick(View v) {
+        String message = "Hello wearable " + num;
+        //Requires a new thread to avoid blocking the UI
+        sendData(message);
+        num++;
+    }
+
+    /**
+     * Receives the data, note since we are using the same data base, we will also receive
+     * data that we sent as well.  Likely should add some kind of id number to datamap, so it
+     * can tell between mobile device and wear device.  or this maybe the functionality you want.
+     */
+    @Override
+    public void onDataChanged(@NonNull DataEventBuffer dataEventBuffer) {
+        //Log.d(TAG, "onDataChanged: " + dataEventBuffer);
+        for (DataEvent event : dataEventBuffer) {
+            if (event.getType() == DataEvent.TYPE_CHANGED) {
+                DataItem item = event.getDataItem();
+                String path = event.getDataItem().getUri().getPath();
+                if (datapath.equals(path)) {
+                    DataMapItem dataMapItem = DataMapItem.fromDataItem(event.getDataItem());
+                    String message = dataMapItem.getDataMap().getString("message");
+                    Log.v(TAG, "Wear activity received message: " + message);
+                    // Display message in UI
+                    logthis(message);
+                } else if(item.getUri().getPath().compareTo("/count") == 0){
+                    DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                    updateCount(dataMap.getInt("key.count"));
+                } else if (item.getUri().getPath().compareTo("/array") == 0){
+                    DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                    recibeArray(dataMap.getFloatArray("key.array"));
+                } else if(item.getUri().getPath().compareTo("/temp") == 0){
+                    DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                    recibeTemperatura(dataMap.getFloat("temp"));
+                } else if (item.getUri().getPath().compareTo("/hr") == 0){
+                    DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                    recibeHeart(dataMap.getFloat("hr"));
+                }
+                else {
+                    Log.e(TAG, "Unrecognized path: " + path);
+                }
+            } else if (event.getType() == DataEvent.TYPE_DELETED) {
+                Log.v(TAG, "Data deleted : " + event.getDataItem().toString());
+            } else {
+                Log.e(TAG, "Unknown data event Type = " + event.getType());
+            }
+        }
+    }
+
+    private void recibeTemperatura(float temp){
+        String t = Float.toString(temp);
+        txtTemp.setText(t);
+
+    }
+    private void recibeHeart(float hr){
+        String h = Float.toString(hr);
+        txtHr.setText(h);
+
+    }
+    private void recibeArray(float[] floatArray) {
+        Log.d(TAG, "recibeArray: " + floatArray);
+    }
+
+    private void updateCount(int c) {
+        Log.d(TAG, "updateCount: " + c);
+    }
+
+
+    /**
+     * Sends the data.  Since it specify a client, everyone who is listening to the path, will
+     * get the data.
+     */
+    private void sendData(String message) {
+        PutDataMapRequest dataMap = PutDataMapRequest.create(datapath);
+        dataMap.getDataMap().putString("message", message);
+        PutDataRequest request = dataMap.asPutDataRequest();
+        request.setUrgent();
+
+        Task<DataItem> dataItemTask = Wearable.getDataClient(this).putDataItem(request);
+        dataItemTask
+                .addOnSuccessListener(new OnSuccessListener<DataItem>() {
+                    @Override
+                    public void onSuccess(DataItem dataItem) {
+                        Log.d(TAG, "Sending message was successful: " + dataItem);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "Sending message failed: " + e);
+                    }
+                })
+        ;
+    }
+}
---
+++ wear/src/main/java/com/jp/wear/phone/mywear/MainActivity.java
@@ -0,0 +1,279 @@
+package com.jp.wear.phone.mywear;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+
+import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+
+import com.google.android.gms.wearable.CapabilityClient;
+import com.google.android.gms.wearable.CapabilityInfo;
+import com.google.android.gms.wearable.DataClient;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.MessageClient;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+
+public class MainActivity extends Activity
+        implements
+        DataClient.OnDataChangedListener
+        /*MessageClient.OnMessageReceivedListener,
+        CapabilityClient.OnCapabilityChangedListener,
+        View.OnClickListener*/ {
+
+    private final static String TAG = "Wear MainActivity";
+    private TextView mTextView;
+    Button myButton;
+    int num = 1;
+    String datapath = "/data_path";
+    float[] signosV = new float[4];
+    private int count = 0;
+    private TextView txtHr,txtTemp;
+    Sensor mHeartR, mTemp;
+    SensorManager sensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtHr = findViewById(R.id.txtHr);
+        txtTemp = findViewById(R.id.txtTemp);
+
+        //Inicializar sensores
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mHeartR = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+        if (mHeartR != null){
+            sensorManager.registerListener(mSensorEventListener, mHeartR, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else {
+            txtHr.setText("HR not supported");
+        }
+        mTemp = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        if (mTemp != null){
+            sensorManager.registerListener(mSensorEventListener, mTemp, SensorManager.SENSOR_DELAY_NORMAL);
+        }else{
+            txtTemp.setText("Temp not supported");
+        }
+
+        mTextView = findViewById(R.id.text);
+        //send a message from the wear.  This one will not have response.
+        myButton = findViewById(R.id.wrbutton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "Hello device " + num;
+                //Requires a new thread to avoid blocking the UI
+                sendData(message);
+                increaseCounter();
+                sendArray();
+                num++;
+            }
+        });
+        // Enables Always-on
+        //setAmbientEnabled();
+    }
+
+
+
+    //add listener.
+    @Override
+    public void onResume() {
+        super.onResume();
+        Wearable.getDataClient(this).addListener(this);
+    }
+
+    //remove listener
+    @Override
+    public void onPause() {
+        super.onPause();
+        Wearable.getDataClient(this).removeListener(this);
+    }
+
+    //receive data from the path.
+    @Override
+    public void onDataChanged(@NonNull DataEventBuffer dataEventBuffer) {
+        Log.d(TAG, "onDataChanged: " + dataEventBuffer);
+        for (DataEvent event : dataEventBuffer) {
+            if (event.getType() == DataEvent.TYPE_CHANGED) {
+                String path = event.getDataItem().getUri().getPath();
+                if (datapath.equals(path)) {
+                    DataMapItem dataMapItem = DataMapItem.fromDataItem(event.getDataItem());
+                    String message = dataMapItem.getDataMap().getString("message");
+                    Log.v(TAG, "Wear activity received message: " + message);
+                    // Display message in UI
+                    mTextView.setText(message);
+
+                } else if (path.equals("/count")){
+
+
+                } else if (path.equals("/hr")){
+
+                }
+                else {
+                    Log.e(TAG, "Unrecognized path: " + path);
+                }
+            } else if (event.getType() == DataEvent.TYPE_DELETED) {
+                Log.v(TAG, "Data deleted : " + event.getDataItem().toString());
+            } else {
+                Log.e(TAG, "Unknown data event Type = " + event.getType());
+            }
+        }
+    }
+
+    /**
+     * Sends the data, note this is a broadcast, so we will get the message as well.
+     */
+    private void sendData(String message) {
+        PutDataMapRequest dataMap = PutDataMapRequest.create(datapath);
+        dataMap.getDataMap().putString("message", message);
+        PutDataRequest request = dataMap.asPutDataRequest();
+        request.setUrgent();
+
+        Task<DataItem> dataItemTask = Wearable.getDataClient(this).putDataItem(request);
+        dataItemTask
+                .addOnSuccessListener(new OnSuccessListener<DataItem>() {
+                    @Override
+                    public void onSuccess(DataItem dataItem) {
+                        Log.d("Mensaje", "Sending message was successful: " + dataItem);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "Sending message failed: " + e);
+                    }
+                })
+        ;
+    }
+
+    private void sendHR(float hr){
+        PutDataMapRequest dataMap = PutDataMapRequest.create("/hr");
+        dataMap.getDataMap().putFloat("hr", hr);
+        PutDataRequest request = dataMap.asPutDataRequest();
+        request.setUrgent();
+        Task<DataItem> dataItemTask = Wearable.getDataClient(this).putDataItem(request);
+        dataItemTask
+                .addOnSuccessListener(new OnSuccessListener<DataItem>() {
+                    @Override
+                    public void onSuccess(DataItem dataItem) {
+                        Log.d("Heart rate", "Dato enviado");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(Exception e) {
+                        Log.e(TAG, "Sending message failed: " + e);
+                    }
+                });
+
+    }
+    private void sendTemp(float temp){
+        PutDataMapRequest dataMap = PutDataMapRequest.create("/temp");
+        dataMap.getDataMap().putFloat("temp", temp);
+        PutDataRequest request = dataMap.asPutDataRequest();
+        request.setUrgent();
+        Task<DataItem> dataItemTask = Wearable.getDataClient(this).putDataItem(request);
+        dataItemTask
+                .addOnSuccessListener(new OnSuccessListener<DataItem>() {
+                    @Override
+                    public void onSuccess(DataItem dataItem) {
+                        Log.d("temperatura", "Dato enviado");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "Sending message failed: " + e);
+                    }
+                });
+    }
+
+    private void increaseCounter(){
+        PutDataMapRequest putDataMapReq = PutDataMapRequest.create("/count");
+        putDataMapReq.getDataMap().putInt("key.count", count++);
+        PutDataRequest putDataReq = putDataMapReq.asPutDataRequest();
+        Task<DataItem> putDataTask = Wearable.getDataClient(this).putDataItem(putDataReq);
+        putDataTask.addOnSuccessListener(new OnSuccessListener<DataItem>() {
+            @Override
+            public void onSuccess(DataItem dataItem) {
+                Log.d("Couter", "onSuccess: " + dataItem);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+    private void sendArray(){
+        signosV[0] = 10.1f;
+        signosV[1] = 20.2f;
+        signosV[2] = 30.3f;
+        signosV[3] = 40.4f;
+        PutDataMapRequest putDataMapReq = PutDataMapRequest.create("/array");
+        putDataMapReq.getDataMap().putFloatArray("key.array", signosV);
+        PutDataRequest putDataReq = putDataMapReq.asPutDataRequest();
+        Task<DataItem> putDataTask = Wearable.getDataClient(this).putDataItem(putDataReq);
+        putDataTask.addOnSuccessListener(new OnSuccessListener<DataItem>() {
+            @Override
+            public void onSuccess(DataItem dataItem) {
+                Log.d("Array", "onSuccess: " + dataItem);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("Array", "onFailure: " + e);
+            }
+        });
+    }
+
+
+    private SensorEventListener mSensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            Sensor sensor = sensorEvent.sensor;
+
+            if (sensor.getType() == Sensor.TYPE_HEART_RATE){
+                float hr = sensorEvent.values[0];
+                txtHr.setText("HR: " + hr);
+                sendHR(hr);
+
+            } else if (sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE){
+                float temp = sensorEvent.values[0];
+                txtTemp.setText("Temp: "+ temp);
+                sendTemp(temp);
+            }
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
+
+
+}
+
