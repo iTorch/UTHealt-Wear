a9ac9486a09bc80db699b22258084b0a0a401c34
Juan Montiel <3518110531@uth.edu.mx>
2021-08-19 19:07:51 -0500

    Final

---
+++ mobile/src/main/java/com/jp/wear/phone/mywear/Body/LogoutBody.java
@@ -0,0 +1,13 @@
+package com.jp.wear.phone.mywear.Body;
+
+public class LogoutBody {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ mobile/src/main/java/com/jp/wear/phone/mywear/Model/Logout.java
@@ -0,0 +1,9 @@
+package com.jp.wear.phone.mywear.Model;
+
+public class Logout {
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+}
